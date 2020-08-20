5624786cf7f46f18532a9702dcad454128c354dc
praveen12math <praveen12math@github.com>
2020-08-21 01:19:51 +0530

    Updates

---
+++ precache-manifest.6c73499cdab27315bfafd4af16b3eaaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73413228b9152216fd41526537d3707",
+    "url": "/GitHub-Api/index.html"
+  },
+  {
+    "revision": "46f2f65a67753f82a3fa",
+    "url": "/GitHub-Api/static/css/main.584b7425.chunk.css"
+  },
+  {
+    "revision": "4d4d3b7d9b0a2e8580cc",
+    "url": "/GitHub-Api/static/js/2.f2ecbfd1.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/GitHub-Api/static/js/2.f2ecbfd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f2f65a67753f82a3fa",
+    "url": "/GitHub-Api/static/js/main.449fb42b.chunk.js"
+  },
+  {
+    "revision": "b3991714980d20a7b5a4",
+    "url": "/GitHub-Api/static/js/runtime-main.0040e770.js"
+  }
+]);
